f3c5efe33587d4055210db7e8f8b3f1b2a0fe770
Andrew Stelter <andrew@stelterfamily.net>
2018-02-05 23:01:42 -0700

    Changed default robot loader layout

--- pkg_DefaultRobotLoader/src/default_robot_loader.cpp
+++ pkg_DefaultRobotLoader/src/default_robot_loader.cpp
@@ -3,13 +3,29 @@
 
 QVector<WorldObject*> DefaultRobotLoader::loadFile(QString filePath, QMap<QString, WorldObjectComponent_Plugin_If *> plugins)
 {
+    WorldObject* robot1 = makeDiffDriveBot(plugins);
+    QMap<QString, PropertyView> props = robot1->getProperties();
+    props["X"].set(20, true);
+    props["Y"].set(0, true);
+    props["Theta"].set(0, true);
+
+
+    WorldObject* robot2 = makeAckermannBot(plugins);
+
+    robot2->getProperties()["X"].set(0, true);
+    robot2->getProperties()["Y"].set(0, true);
+    robot2->getProperties()["Theta"].set(90, true);
+
+    return QVector<WorldObject*>{/*robot1,*/ robot2};
+}
+
+WorldObject* DefaultRobotLoader::makeDiffDriveBot(QMap<QString, WorldObjectComponent_Plugin_If *> plugins)
+{
+
     QVector<WorldObjectComponent_If*> components;
 
-    qDebug() << "Building Default Robot";
     if(plugins.contains("org.sdsmt.sim.2d.worldObjectComponent.defaults.touchring"))
     {
-        qDebug() << "Adding touch sensor ring";
-
         WorldObjectComponent_If* touchRing = plugins["org.sdsmt.sim.2d.worldObjectComponent.defaults.touchring"]->createComponent();
         QMap<QString, PropertyView> props = touchRing->getProperties();
 
@@ -26,8 +42,6 @@ QVector<WorldObject*> DefaultRobotLoader::loadFile(QString filePath, QMap<QStrin
 
     if(plugins.contains("org.sdsmt.sim.2d.worldObjectComponent.defaults.fixedwheel"))
     {
-        qDebug() << "Adding differential wheel base";
-
         WorldObjectComponent_If* leftWheel = plugins["org.sdsmt.sim.2d.worldObjectComponent.defaults.fixedwheel"]->createComponent();
         WorldObjectComponent_If* rightWheel = plugins["org.sdsmt.sim.2d.worldObjectComponent.defaults.fixedwheel"]->createComponent();
 
@@ -64,8 +78,6 @@ QVector<WorldObject*> DefaultRobotLoader::loadFile(QString filePath, QMap<QStrin
 
     if(plugins.contains("org.sdsmt.sim.2d.worldObjectComponent.defaults.circle"))
     {
-        qDebug() << "Adding simple circle";
-
         WorldObjectComponent_If* circle = plugins["org.sdsmt.sim.2d.worldObjectComponent.defaults.circle"]->createComponent();
         QMap<QString, PropertyView> props = circle->getProperties();
 
@@ -76,15 +88,20 @@ QVector<WorldObject*> DefaultRobotLoader::loadFile(QString filePath, QMap<QStrin
         components.push_back(circle);
     }
 
+    return new WorldObject(components);
+}
+
+WorldObject* DefaultRobotLoader::makeAckermannBot(QMap<QString, WorldObjectComponent_Plugin_If *> plugins)
+{
+    QVector<WorldObjectComponent_If*> components;
+
     if(plugins.contains("org.sdsmt.sim.2d.worldObjectComponent.defaults.lidar"))
     {
-        qDebug() << "Adding lidar";
-
         WorldObjectComponent_If* lidar = plugins["org.sdsmt.sim.2d.worldObjectComponent.defaults.lidar"]->createComponent();
         QMap<QString, PropertyView> props = lidar->getProperties();
 
         props["x_local"].set(0, true);
-        props["y_local"].set(1, true);
+        props["y_local"].set(2, true);
         props["scan_radius"].set(5, true);
         props["scan_range"].set(90, true);
         props["scan_points"].set(20, true);
@@ -95,32 +112,71 @@ QVector<WorldObject*> DefaultRobotLoader::loadFile(QString filePath, QMap<QStrin
 
     if(plugins.contains("org.sdsmt.sim.2d.worldObjectComponent.defaults.rectangle"))
     {
-        qDebug() << "Adding rectangle";
-
         WorldObjectComponent_If* rectangle = plugins["org.sdsmt.sim.2d.worldObjectComponent.defaults.rectangle"]->createComponent();
         QMap<QString, PropertyView> props = rectangle->getProperties();
 
         props["x_pos"].set(0, true);
-        props["y_pos"].set(-1, true);
-        props["height"].set(1, true);
+        props["y_pos"].set(0, true);
+        props["height"].set(4, true);
         props["width"].set(2, true);
-        props["rotation"].set(45, true);
+        props["rotation"].set(0, true);
 
         components.push_back(rectangle);
     }
 
-    WorldObject* robot1 = new WorldObject(components);
-    QMap<QString, PropertyView> props = robot1->getProperties();
-    props["X"].set(20, true);
-    props["Y"].set(0, true);
-    props["Theta"].set(0, true);
+    if(plugins.contains("org.sdsmt.sim.2d.worldObjectComponent.defaults.fixedwheel"))
+    {
+        WorldObjectComponent_If* leftWheel = plugins["org.sdsmt.sim.2d.worldObjectComponent.defaults.fixedwheel"]->createComponent();
+        WorldObjectComponent_If* rightWheel = plugins["org.sdsmt.sim.2d.worldObjectComponent.defaults.fixedwheel"]->createComponent();
 
+        QMap<QString, PropertyView> propsl = leftWheel->getProperties();
+        QMap<QString, PropertyView> propsr = rightWheel->getProperties();
 
-    WorldObject* robot2 = new WorldObject(components);
+        propsl["x_local"].set(-1.25, true);
+        propsr["x_local"].set(1.25, true);
 
-    robot2->getProperties()["X"].set(0, true);
-    robot2->getProperties()["Y"].set(0, true);
-    robot2->getProperties()["Theta"].set(315, true);
+        propsl["y_local"].set(-2, true);
+        propsr["y_local"].set(-2, true);
 
-    return QVector<WorldObject*>{robot1, robot2};
+        propsl["theta_local"].set(90, true);
+        propsr["theta_local"].set(90, true);
+
+        propsl["wheel_radius"].set(0.75, true);
+        propsr["wheel_radius"].set(0.75, true);
+
+        propsl["wheel_width"].set(0.5, true);
+        propsr["wheel_width"].set(0.5, true);
+
+        propsl["density"].set(20, true);
+        propsr["density"].set(20, true);
+
+        propsl["is_driven"].set(true, true);
+        propsr["is_driven"].set(true, true);
+
+        propsl["channels/input_speed"].set("robot1/left_wheel", true);
+        propsr["channels/input_speed"].set("robot1/right_wheel", true);
+
+        components.push_back(leftWheel);
+        components.push_back(rightWheel);
+    }
+
+    if(plugins.contains("org.sdsmt.sim.2d.worldObjectComponent.defaults.ackermann"))
+    {
+        WorldObjectComponent_If* steer = plugins["org.sdsmt.sim.2d.worldObjectComponent.defaults.ackermann"]->createComponent();
+
+        QMap<QString, PropertyView> props = steer->getProperties();
+
+        props["x_local"].set(0, true);
+        props["y_local"].set(2, true);
+        props["vehicle_length"].set(4, true);
+        props["axle_length"].set(2.5, true);
+        props["theta_local"].set(0, true);
+        props["wheel_radius"].set(0.75, true);
+        props["wheel_width"].set(0.5, true);
+        props["density"].set(20, true);
+
+        components.push_back(steer);
+    }
+
+    return new WorldObject(components);
 }
